d91c78993d54493d7dd1f507f156bbf3223ac5ed
tharudew7 <73303151+tharudew7@users.noreply.github.com>
2020-11-10 01:02:51 +0530

    edited

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login and registration form</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="login-box">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Login Here</h2>
+				<form action="validation.php" method="post">
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" name="user" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="user" class="form-control" required>
+					</div>
+					<button type="submit" class="btn btn-primary">Login</button>
+				</form>
+			</div>
+			<div class="col-md-6">
+				<h2>Register Here</h2>
+				<form action="registration.php" method="post">
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" name="user" class="form-control" required>
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" name="user" class="form-control" required>
+					</div>
+					<button type="submit" class="btn btn-primary">Register</button>
+				</form>
+			</div>
+		</div>
+		</div>
+	</div>
+
+</body>
+</html>
